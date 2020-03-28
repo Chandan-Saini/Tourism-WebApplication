6eeb6273ba76f1ec750211e2915babf35186d2a8
EC2 Default User <ec2-user@ip-172-31-15-107.ap-south-1.compute.internal>
2020-03-28 13:51:23 +0000

    this is new app tourism

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var commentRoutes    = require("./routes/comments") ,
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
    
-var url= process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp_v12"      //this line is used so that if our local server restarts or having some connection problem with the local env var then it can connect to the url given 
+var url= process.env.DATABASEURLT || "mongodb://localhost:27017/yelp_camp_v12"      //this line is used so that if our local server restarts or having some connection problem with the local env var then it can connect to the url given 
 mongoose.connect(url)   //this line heping our app to connect to local DB server as well as hosted DB at mongodb atlas  
 
 
